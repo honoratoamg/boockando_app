6ce4cb1ced93ce62bc0e3d243eee17923bc0b771
honoratoamg <69965835+honoratoamg@users.noreply.github.com>
2021-03-19 09:15:17 -0300

    Some fix

--- lib/app/modules/login/login_page.dart
+++ lib/app/modules/login/login_page.dart
@@ -42,106 +42,114 @@ class _State extends State<LoginPage> {
                       curve: Curves.linear,
                       opacity: !controller.isActionSuccess.value ? 0.0 : 1.0,
                       duration: Duration(milliseconds: 1000),
-                      child: Container(
-                        alignment: Alignment.center,
-                        child: Column(
-                          children: <Widget>[
-                            Icon(
-                              Icons.menu_book,
-                              size: 80,
-                            ),
-                            Text("Welcome to Boockando, ${userName}!")
-                          ],
-                        ),
-                      ),
+                      child: animatedFeedbackWidget(),
                     ),
                   ),
                   AnimatedOpacity(
                     curve: Curves.bounceIn,
                     opacity: controller.isActionSuccess.value ? 0 : 1.0,
                     duration: Duration(milliseconds: 0),
-                    child: Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Form(
-                        key: _formKey,
-                        child: ListView(
-                          children: <Widget>[
-                            Container(
-                                alignment: Alignment.topLeft,
-                                padding: EdgeInsets.all(10),
-                                child: Center(
-                                  child: Text(
-                                    'Login',
-                                  ),
-                                )),
-                            Container(
-                              padding: EdgeInsets.all(10),
-                              child: TextFormField(
-                                controller: controller.nameController,
-                                onChanged: (valor) => setState(() =>
-                                    userName = valor.trim().toLowerCase()),
-                                decoration: InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  labelText: 'User',
-                                ),
-                                validator: (String submittedValue) {
-                                  if (submittedValue.isEmpty) {
-                                    return "This field can't be empty!";
-                                  }
-                                  return null;
-                                },
-                              ),
-                            ),
-                            SizedBox(height: 5),
-                            Container(
-                              padding: EdgeInsets.all(10),
-                              child: TextFormField(
-                                controller: controller.passwordController,
-                                obscureText: true,
-                                enableSuggestions: false,
-                                autocorrect: false,
-                                onChanged: (valor) =>
-                                    setState(() => password = valor),
-                                decoration: InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  labelText: 'Password',
-                                ),
-                                validator: (String submittedValue) {
-                                  if (submittedValue.isEmpty) {
-                                    return "'This field can't be empty!";
-                                  }
-                                  return null;
-                                },
-                              ),
-                            ),
-                            SizedBox(height: 5),
-                            Container(
-                              height: 50,
-                              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                              child: RaisedButton(
-                                textColor: Colors.black,
-                                child: Text('Enter'),
-                                onPressed: () async {
-                                  if (_formKey.currentState.validate()) {
-                                    controller.loginUser(
-                                        context, userName, password);
-                                  }
-                                },
-                              ),
-                            ),
-                            SizedBox(height: 7),
-                            FlatButton(
-                                onPressed: () {
-                                  Modular.link.pushNamed(SignUpPage.routeName);
-                                },
-                                child: Text("Not registered? Sign up here!")),
-                          ],
-                        ),
-                      ),
-                    ),
+                    child: loginWidget(),
                   ),
                 ]),
           );
         });
+  }
+
+  // Login Widget
+  loginWidget() {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          children: <Widget>[
+            Container(
+                alignment: Alignment.topLeft,
+                padding: EdgeInsets.all(10),
+                child: Center(
+                  child: Text(
+                    'Login',
+                  ),
+                )),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextFormField(
+                controller: controller.nameController,
+                onChanged: (valor) =>
+                    setState(() => userName = valor.trim().toLowerCase()),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'User',
+                ),
+                validator: (String submittedValue) {
+                  if (submittedValue.isEmpty) {
+                    return "This field can't be empty!";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            SizedBox(height: 5),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextFormField(
+                controller: controller.passwordController,
+                obscureText: true,
+                enableSuggestions: false,
+                autocorrect: false,
+                onChanged: (valor) => setState(() => password = valor),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+                validator: (String submittedValue) {
+                  if (submittedValue.isEmpty) {
+                    return "'This field can't be empty!";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            SizedBox(height: 5),
+            Container(
+              height: 50,
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: RaisedButton(
+                textColor: Colors.black,
+                child: Text('Enter'),
+                onPressed: () async {
+                  if (_formKey.currentState.validate()) {
+                    controller.loginUser(context, userName, password);
+                  }
+                },
+              ),
+            ),
+            SizedBox(height: 7),
+            FlatButton(
+                onPressed: () {
+                  Modular.link.pushNamed(SignUpPage.routeName);
+                },
+                child: Text("Not registered? Sign up here!")),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Animation feedback Widget
+  animatedFeedbackWidget() {
+    return Container(
+      alignment: Alignment.center,
+      child: Column(
+        children: <Widget>[
+          Icon(
+            Icons.menu_book,
+            size: 80,
+          ),
+          Text("Welcome to Boockando, ${userName}!")
+        ],
+      ),
+    );
   }
 }
--- lib/app/modules/store/page/basket_page.dart
+++ lib/app/modules/store/page/basket_page.dart
@@ -44,102 +44,102 @@ class _BasketPageState extends State<BasketPage> {
               ),
               centerTitle: true,
             ),
-            body: Stack(alignment: AlignmentDirectional.center, children: <
-                Widget>[
-              Positioned(
-                bottom: MediaQuery.of(context).size.height / 2,
-                child: AnimatedOpacity(
-                  curve: Curves.fastOutSlowIn,
-                  opacity: controller.isActionSuccess.value ? 1.0 : 0.0,
-                  duration: Duration(milliseconds: 1000),
-                  child: Container(
-                    alignment: Alignment.center,
-                    child: Column(
-                      children: <Widget>[
-                        Icon(
-                          Icons.done_outline,
-                          color: Colors.green,
-                          size: 80,
-                        ),
-                        Text("Purchase completed!")
-                      ],
+            body: Stack(
+                alignment: AlignmentDirectional.center,
+                children: <Widget>[
+                  Positioned(
+                    bottom: MediaQuery.of(context).size.height / 2,
+                    child: AnimatedOpacity(
+                      curve: Curves.fastOutSlowIn,
+                      opacity: controller.isActionSuccess.value ? 1.0 : 0.0,
+                      duration: Duration(milliseconds: 1000),
+                      child: Container(
+                        alignment: Alignment.center,
+                        child: feedbackWidget(),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              AnimatedOpacity(
-                curve: Curves.bounceIn,
-                opacity: controller.isActionSuccess.value ? 0 : 1.0,
-                duration: Duration(milliseconds: 0),
-                child: Consumer<AppBasketController>(builder: (context, value) {
-                  return (basketController.userBasketBooks.isNotEmpty)
-                      ? Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            alignment: Alignment.topCenter,
-                            child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Consumer<AppBasketController>(
-                                      builder: (context, value) {
-                                    return basketController.userBasketBooks !=
-                                            null
-                                        ? Expanded(
-                                            child: SizedBox(
-                                              height: MediaQuery.of(context)
-                                                  .size
-                                                  .height,
-                                              child: ListView.builder(
-                                                scrollDirection: Axis.vertical,
-                                                shrinkWrap: true,
-                                                itemCount: basketController
-                                                    .userBasketBooks.length,
-                                                itemBuilder:
-                                                    (BuildContext context,
-                                                            int index) =>
-                                                        BookBasketWidget(
-                                                            key: UniqueKey(),
-                                                            index: index),
-                                              ),
-                                            ),
-                                          )
-                                        : CircularProgressIndicator();
-                                  }),
-                                  RaisedButton(
-                                      child: Text('Buy now'),
-                                      onPressed: () async {
-                                        bool isConnectedUser;
-                                        await InternetConnectionChecker
-                                                .checkConnection()
-                                            .then((value) =>
-                                                isConnectedUser = value);
-                                        if (isConnectedUser) {
-                                          controller
-                                              .showAlertConfirmation(context);
-                                        } else {
-                                          AlertsUtils.connectionFailed(context);
-                                        }
-                                      }),
-                                ]),
-                          ),
-                        )
-                      : Center(
-                          child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Icon(Icons.work_off_outlined, size: 60),
-                            Flexible(
-                                child: SingleChildScrollView(
-                                    child:
-                                        Text('Oops..Your basket is empty!'))),
-                          ],
-                        ));
-                }),
-              ),
-            ]),
+                  AnimatedOpacity(
+                    curve: Curves.bounceIn,
+                    opacity: controller.isActionSuccess.value ? 0 : 1.0,
+                    duration: Duration(milliseconds: 0),
+                    child: purchaseWidget(),
+                  ),
+                ]),
           );
         });
+  }
+
+  // Purchase Widget
+  purchaseWidget() {
+    return Consumer<AppBasketController>(builder: (context, value) {
+      return (basketController.userBasketBooks.isNotEmpty)
+          ? Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                alignment: Alignment.topCenter,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Consumer<AppBasketController>(builder: (context, value) {
+                        return basketController.userBasketBooks != null
+                            ? Expanded(
+                                child: SizedBox(
+                                  height: MediaQuery.of(context).size.height,
+                                  child: ListView.builder(
+                                    scrollDirection: Axis.vertical,
+                                    shrinkWrap: true,
+                                    itemCount:
+                                        basketController.userBasketBooks.length,
+                                    itemBuilder:
+                                        (BuildContext context, int index) =>
+                                            BookBasketWidget(
+                                                key: UniqueKey(), index: index),
+                                  ),
+                                ),
+                              )
+                            : CircularProgressIndicator();
+                      }),
+                      RaisedButton(
+                          child: Text('Buy now'),
+                          onPressed: () async {
+                            bool isConnectedUser;
+                            await InternetConnectionChecker.checkConnection()
+                                .then((value) => isConnectedUser = value);
+                            if (isConnectedUser) {
+                              controller.showAlertConfirmation(context);
+                            } else {
+                              AlertsUtils.connectionFailed(context);
+                            }
+                          }),
+                    ]),
+              ),
+            )
+          : Center(
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(Icons.work_off_outlined, size: 60),
+                Flexible(
+                    child: SingleChildScrollView(
+                        child: Text('Oops..Your basket is empty!'))),
+              ],
+            ));
+    });
+  }
+
+  // Purchase feedback animation
+  feedbackWidget() {
+    return Column(
+      children: <Widget>[
+        Icon(
+          Icons.done_outline,
+          color: Colors.green,
+          size: 80,
+        ),
+        Text("Purchase completed!")
+      ],
+    );
   }
 }
